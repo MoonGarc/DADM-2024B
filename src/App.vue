cbb8f7836e4db899a9dce9ca43530c31c88b7744
MoonGarc <159564300+MoonGarc@users.noreply.github.com>
2024-10-01 21:02:32 +0000

    ⭐FEAT: Eliminando funcionalidad de helados

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,28 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { ref } from "vue";
+
+const header = ref("🛒 Shopping List App");
+const items = ref([
+  { id: 1, label: "10 bolillos" },
+  { id: 2, label: "1 lata de frijoles" },
+  { id: 3, label: "2 lata de atún" },
+]);
+const newItem = ref("");
+const newItemHighPriority = ref(false);
+</script>
+
 <template>
-    <h1>
-      <i class="material-icons shopping-cart-icon">local_mall</i>
-      {{ header }}
-    </h1>
-    <input type="text" placeholder="Add Item" v-model.trim="newItem">
-    
-    <!-- Caja de seleccion de prioridad -->
-    <label>
-      <input type="checkbox" v-model="newItemHighPriority">
-      High Priority
-    </label>
-    
-    <!-- Helados -->
-    <label>
-      <input type="checkbox" v-model="iceCreamFlavors" value="vanilla">
-      Vanilla
-    </label>
-    <label>
-      <input type="checkbox" v-model="iceCreamFlavors" value="chocolate">
-      Chocolate
-    </label>
-    <label>
-      <input type="checkbox" v-model="iceCreamFlavors" value="strawberry">
-      Strawberry
-    </label>
-  
-    {{ iceCreamFlavors }}
-  
-    <!-- Lista -->
-    <ul>
-      <li v-for="{ id, label } in items" v-bind:key="id">
-        ⚜ {{ label }}
-      </li>
-    </ul>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  
-  const header = ref('App lista de compras');
-  const items = ref([
-    { id: '0', label: '10 bolillos' },  
-    { id: '1', label: '1 lata frijoles' },
-    { id: '2', label: '1 Chelas' },
-    { id: '3', label: '1 nutella' },
-  ]);
-  const newItem = ref('');
-  const newItemHighPriority = ref(false);
-  const iceCreamFlavors = ref([]);
-  </script>
-  
-  <style scoped>
-  .shopping-cart-icon {
-    font-size: 2rem;
-  }
-  </style>
-  
+  <h1>{{ header }}</h1>
+  <input v-model.trim="newItem" type="text" placeholder="Add Item" />
+  <!-- Caja de seleccion de prioridad -->
+  <label>
+    <input type="checkbox" v-model="newItemHighPriority">
+    High Priority
+  </label>
+  <!-- Lista -->
+  <ul>
+    <li v-for="({ id, label }, index) in items" key="id">⚜ {{ label }}</li>
+  </ul>
+</template>
