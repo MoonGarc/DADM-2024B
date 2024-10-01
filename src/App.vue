3e130761d384abc1e6930f019106124260a9fd2b
MoonGarc <159564300+MoonGarc@users.noreply.github.com>
2024-10-01 19:53:33 +0000

    ⭐FEAT: Creando un input

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,33 @@
-<script setup>
-import { ref } from 'vue';
-//Modelo
-const header = ref  ('App lista de compras');
-const items = ref ([
-{id: '0', label: '10 bolillos'},  
-{id: '1', label:'1 lata frijoles'},
-{id: '2', label: '1 Chelas'},
-{id: '3', label: '1 nutella'},
-]);
-const newItem = ref('');
-const newItemPriority = ref('low');
-</script>
-
 <template>
-<h1> 
-    <i class="material-icons shopping-cart-icon"> local_mall</i>
-    {{ header }} 
-    </h1>
-    <input v-model.lazy="newItem" type="text" placeholder="Agregar Articulo">
-    <!--Radio Buttons-->
-    <label><input type="radio" value="low" v-model="newItemPriority">baja</label>
-    <label><input type="radio" value="high" v-model="newItemPriority">alta</label>
-    {{ newItemPriority == 'low' ? '🧊' : '🔥' }}    
+    <h1>{{ header }}</h1>
+    <input 
+      type="text" 
+      v-model="newItem" 
+      placeholder="Add Item"
+    />
     <ul>
-        <li v-for= "({id, label}, i) in items" :key = "id">{{i+1}} {{ i%2==0?'🎇': '👜'}} {{ label }} </li>
+      <li v-for="({ id, label }, index) in items" v-bind:key="id">
+        ⚜ {{ label }}
+      </li>
     </ul>
-</template>
-
-<style scoped>
-.shopping-cart-icon {
-font-size: 2rem;
-}
-</style>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const header = ref('App lista de compras');
+  const items = ref([
+    { id: '0', label: '10 bolillos' },  
+    { id: '1', label: '1 lata frijoles' },
+    { id: '2', label: '1 Chelas' },
+    { id: '3', label: '1 nutella' },
+  ]);
+  const newItem = ref('');
+  </script>
+  
+  <style scoped>
+  .shopping-cart-icon {
+    font-size: 2rem;
+  }
+  </style>
+  
