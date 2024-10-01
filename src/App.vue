9421e48b353cccbb47f93d6886d1b860cddd2240
MoonGarc <159564300+MoonGarc@users.noreply.github.com>
2024-10-01 21:17:48 +0000

    ⭐FEAT: Eventos de usuario

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// Este código está utilizando Vue 3 <script setup>
+// Más información en https://vuejs.org/api/sfc-script-setup.html#script-setup
 import { ref } from "vue";
 
 const header = ref("🛒 Shopping List App");
 const items = ref([
   { id: 1, label: "10 bolillos" },
   { id: 2, label: "1 lata de frijoles" },
-  { id: 3, label: "2 lata de atún" },
+  { id: 3, label: "2 latas de atún" },
 ]);
 const newItem = ref("");
 const newItemHighPriority = ref(false);
@@ -15,14 +15,27 @@ const newItemHighPriority = ref(false);
 
 <template>
   <h1>{{ header }}</h1>
-  <input v-model.trim="newItem" type="text" placeholder="Add Item" />
-  <!-- Caja de seleccion de prioridad -->
-  <label>
-    <input type="checkbox" v-model="newItemHighPriority">
-    High Priority
-  </label>
+  
+  <!-- Agrupando entradas de usuario -->
+  <div class="add-item form">
+    <!-- Entrada de texto -->
+    <input type="text" placeholder="Add Item" v-model.trim="newItem" />
+    
+    <!-- Caja de selección de prioridad -->
+    <label>
+      <input type="checkbox" v-model="newItemHighPriority" /> Alta Prioridad
+    </label>
+    
+    <!-- Botón -->
+    <button 
+      class="btn btn-primary" 
+      v-on:click="items.push({ id: items.length + 1, label: newItem })">
+      Salvar Artículo
+    </button>
+  </div>
+
   <!-- Lista -->
   <ul>
-    <li v-for="({ id, label }, index) in items" key="id">⚜ {{ label }}</li>
+    <li v-for="({ id, label }) in items" :key="id">⚜ {{ label }}</li>
   </ul>
 </template>
