ab6e164edb5ff6fc8d0656e45ad934829517e72b
MoonGarc <159564300+MoonGarc@users.noreply.github.com>
2024-10-09 17:55:35 +0000

    ⭐S13 Clases Estáticas

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,10 @@ const header = ref('App lista de compras');
 
 // ---items----
 const items = ref([
-  { id: '0', label: '10 bolillos' },
-  { id: '1', label: '1 chela' },
-  { id: '2', label: 'leche' },
-  { id: '3', label: '1 nutella' }
+  { id: '0', label: '10 bolillos',purchased: false, priority: true },
+  { id: '1', label: '1 chela', purchased: true, priority: true},
+  { id: '2', label: 'leche', purchased: false, priority: false },
+  { id: '3', label: '1 nutella', purchased: true, priority: true },
 ]);
 
 // Item-Method
@@ -59,7 +59,13 @@ const activeEdition = (activate) => {
     </button>
   </form>
   <ul>
-    <li v-for="item in items" :key="item.id"> 🛍️ {{ item.label }} </li>
+    <li 
+    v-for="{label, id, purchased, priority} in items" 
+    :key="id"
+    class="amazing"
+    :class="{ strikeout: purchased, priority: priority}">
+    {{ priority ? "🔥": "🛍️"}} {{label}} 
+    </li>
   </ul>
   <p v-if="items.length === 0"> 🥀 NO HAY ELEMENTOS EN LA LISTA 🥀</p>
 </template>
