3e9407a784d511f2dd545836a15312fed06f111b
MoonGarc <159564300+MoonGarc@users.noreply.github.com>
2024-11-20 18:50:54 +0000

    🚧 S03S06 Hooks

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,6 +1,6 @@
 <script setup>
  import PlanPickerItem from './PlanPickerItem.vue';
- import { ref } from 'vue';
+ import {ref, onMounted} from 'vue';
  //Creando una referencia reactiva
  //para almacenar el plan seleccionado
  const selectedPlan = ref(null)
@@ -10,6 +10,13 @@
   //Almacenar el plan seleccionado
 selectedPlan.value = name;
    };
+   // Intentando obtener la referencia al elemento .plans
+   console.log("Termina de ejecutar el script setup 🥳");
+
+   //registrando el hook mounted 
+   onMounted(() =>{
+    console.log("El componente planPicker se ha montado 🎉");
+   });
 </script>
 
 <template>
