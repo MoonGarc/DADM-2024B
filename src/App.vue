ea76f09e8e5262f17aad1aab890710585194bd2b
MoonGarc <159564300+MoonGarc@users.noreply.github.com>
2024-11-27 17:57:02 +0000

    ⭐S03S06 Hooks

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@ const showPlans = ref(false);
 
 <template>
   <header>
-    <div>
-      <span id="logo">ITGAM BOX EXPERIENCES</span>
+    <div style="text-align:">
+      <span id="logo">ITGAM BOX EXPERIENCIES</span> <br>
       <img src="./assets/logo.png" alt="logo">
     </div>
   </header>
@@ -19,42 +19,13 @@ const showPlans = ref(false);
       Viajamos por el mundo para encontrar el mejor café de origen único para ti
     </h2>
 
-    <label>
-      <input type="checkbox" v-model="showPlans">
+    <label> 
+      <input type="checkbox" v-model="showPlans"> 
       Mostrar selector de planes
     </label>
 
-   <PlanPicker :v-if="showPlans"/>
-  
-
+    <PlanPicker v-if="showPlans" />
+    
   </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
