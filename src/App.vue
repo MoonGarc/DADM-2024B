b6979a2c5a4b5dc5c9ec6573ddf667383b6ae7a6
MoonGarc <159564300+MoonGarc@users.noreply.github.com>
2024-12-03 23:06:40 +0000

    ⭐Introduccion Slots

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,12 @@
 <script setup>
-import PlanPicker from './components/PlanPicker.vue';
-import { ref } from 'vue';
-const showPlans = ref(false);
+import fancyButton from './components/FancyButton.vue';
 </script>
 
 <template>
-  <header>
-    <div style="text-align:">
-      <span id="logo">ITGAM BOX EXPERIENCIES</span> <br>
-      <img src="./assets/logo.png" alt="logo">
-    </div>
-  </header>
-
   <div class="content">
-    <h1 class="title">Coffee Plans</h1>
-
-    <h2 class="subtitle">
-      Viajamos por el mundo para encontrar el mejor café de origen único para ti
-    </h2>
-
-    <label> 
-      <input type="checkbox" v-model="showPlans"> 
-      Mostrar selector de planes
-    </label>
-
-    <PlanPicker v-if="showPlans" />
-    
+    <fancy-button />
   </div>
 </template>
-<style scoped></style>
+
+<style scoped>
+</style>
