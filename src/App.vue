3e0dbeceb60a59ec82c6792fce37028effb21895
MoonGarc <159564300+MoonGarc@users.noreply.github.com>
2024-12-04 00:44:47 +0000

    ⭐Slots multiples

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import FancyButton from './components/FancyButton.vue';
 <template>
   <div class="content">
     <fancy-button>
-      Submit 
+      Hazme Clic
     </fancy-button>
   </div>
 </template>
