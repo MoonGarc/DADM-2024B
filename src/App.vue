3555848489f67db62acc076f791af98cb0582c02
MoonGarc <159564300+MoonGarc@users.noreply.github.com>
2024-12-04 01:08:25 +0000

    🌟Efecto final

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@ import FancyButton from './components/FancyButton.vue';
 <template>
   <div class="content">
     <fancy-button>
+      <template #icon="{ hover }">
+        {{ hover ? "🌟" : "⭐"}}
+      </template>
       Hazme Clic
     </fancy-button>
   </div>
--- src/components/FancyButton.vue
+++ src/components/FancyButton.vue
@@ -2,11 +2,11 @@
     <button 
       @mouseenter="hover = true"
       @mouseleave="hover = false"> 
-      <slot name="icon">➡️</slot>
+      <slot name="icon" :hover="hover">➡️</slot>
       <slot>Submit</slot>
     </button>
-    {{ hover }}
   </template>
+
   
   <script setup>
   import { ref } from 'vue';
