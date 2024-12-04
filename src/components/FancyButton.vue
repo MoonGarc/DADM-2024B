546460854cb60711c71c5f348723efbcde5fcd3e
MoonGarc <159564300+MoonGarc@users.noreply.github.com>
2024-12-04 00:48:15 +0000

    ⭐Scoped slots

--- src/components/FancyButton.vue
+++ src/components/FancyButton.vue
@@ -1,6 +1,14 @@
 <template>
-    <button> 
+    <button 
+      @mouseenter="hover = true"
+      @mouseleave="hover = false"> 
       <slot name="icon">➡️</slot>
       <slot>Submit</slot>
     </button>
+    {{ hover }}
   </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  const hover = ref(false);
+  </script>
